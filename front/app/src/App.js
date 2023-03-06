5e33c1e5136237da01b3425045dd006e360cdfdf
Sebastian Manco Gomez <sebasmg1393@gmail.com>
2023-03-06 16:53:35 -0500

    conflic from master ok, and marge DONE

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,74 +1,13 @@
-import './App.css';
-import { useState } from 'react';
+import './App.scss';
+import UserDataFrom from './components/newUserAdded/UserDataForm';
+import NavBarComponent from './components/navBar/NavBar';
 
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [mobileNumber, setMobileNumber] = useState('');
-  const [message, setMessage] = useState('');
-
-  // console.log('la rama funciona....');
-
-  // probar volumens
-
-  let handleSubmit = async e => {
-    e.preventDefault();
-    try {
-      let res = await fetch('/clients', {
-        method: 'POST',
-        headers:{
-          'accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          cc: '2222',
-          first_name: name,
-          last_name: '2222',
-          cellphone: mobileNumber,
-          email: email,
-        }),
-      });
-      let resJson = await res.json();
-      if (resJson.status === 200) {
-        setName('');
-        setEmail('');
-        setMessage('User created successfully');
-      } else {
-        setMessage('Some error occured');
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <div className="App">
-      <h1>Hello World 🌎</h1>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          placeholder="Name"
-          onChange={e => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          value={email}
-          placeholder="Email"
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          value={mobileNumber}
-          placeholder="Mobile Number"
-          onChange={e => setMobileNumber(e.target.value)}
-        />
-
-        <button type="submit">Create</button>
-
-        <div className="message">{message ? <p>{message}</p> : null}</div>
-      </form>
+      <NavBarComponent></NavBarComponent>
+      {/* <h1>Hello World 🌎</h1> */}
+      <UserDataFrom></UserDataFrom>
     </div>
   );
 }
