cf126254aa83bb7d3b7692249c35e133346a87e7
Sebastian Manco Gomez <sebasmg1393@gmail.com>
2023-08-16 18:02:14 -0500

    seb: form format init

--- front/app/src/components/UserFormData.jsx
+++ front/app/src/components/UserFormData.jsx
@@ -1,7 +1,59 @@
+// import { createPortal } from 'react-dom';
+
+import styles from './UserFormData.module.css';
+
 function UserFormData() {
   return (
-    <div>
-      <h2>Here you put the form</h2>
+    <div className={styles.popup}>
+      <div className={styles.content}>
+        <div>
+          <div>
+            <h2>We offer a free sample meal</h2>
+            <p>
+              Healthy, tasty and hassle-free meals are waiting for you. Start
+              eating well today. You can cancel or pause anytime. And the first
+              meal is on us!
+            </p>
+
+            <form name="sing-up">
+              <div>
+                <label htmlFor="full-name">Full Name</label>
+                <input
+                  id="full-name"
+                  type="text"
+                  placeholder="Sebastian Manco"
+                  name="full-name"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email address</label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="me@example.com"
+                  name="email"
+                  required
+                />
+              </div>
+              <div>
+                <label>Where did you hear from us?</label>
+                <select name="select-where" id="select-where" required>
+                  <option value=""></option>
+                  <option value="frineds">Frineds and family</option>
+                  <option value="youtobe">YouTube videos</option>
+                  <option value="podcast">Podcast</option>
+                  <option value="facebook">Facebook ad</option>
+                  <option value="twitter">Twitter</option>
+                </select>
+              </div>
+
+              <button>Sing up now</button>
+            </form>
+          </div>
+          <div role="img" aria-label="Woman enjoying food"></div>
+        </div>
+      </div>
     </div>
   );
 }
